7762d3e3470175317ceec96435d8c12633c0560b
Adam <adamharald440@gmail.com>
2021-06-16 12:46:34 +0200

    User: Added more attributes

--- src/main/java/com/example/demo/model/admin/User.java
+++ src/main/java/com/example/demo/model/admin/User.java
@@ -1,6 +1,40 @@
 package com.example.demo.model.admin;
 
 public class User {
-    public int playerId;
-    public String playerName;
+    private int playerId;
+    private String playerName;
+    private String playerColor;
+    private int gameId;
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public String getPlayerColor() {
+        return playerColor;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setPlayerColor(String playerColor) {
+        this.playerColor = playerColor;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
 }
