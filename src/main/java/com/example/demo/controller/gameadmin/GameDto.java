8bd99f1d964574198967f46a7db73e548f8ff8ea
Andreas Carlsen <andreas.carlsen98@gmail.com>
2021-06-16 13:14:57 +0200

    GameDto now has a list of users

--- src/main/java/com/example/demo/controller/gameadmin/GameDto.java
+++ src/main/java/com/example/demo/controller/gameadmin/GameDto.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller.gameadmin;
 
+import com.example.demo.controller.GameController.UserDto;
+
+import java.util.ArrayList;
+
 public class GameDto {
 
     private Integer gameId;
     private String name;
+    private UserDto[] users;
 
     public Integer getGameId(){
         return this.gameId;
