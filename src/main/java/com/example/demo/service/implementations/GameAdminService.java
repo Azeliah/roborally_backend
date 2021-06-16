003892702326d0aec25d06adadbe9f8582d1aed1
Adam <adamharald440@gmail.com>
2021-06-16 13:19:49 +0200

    GameAdminService: Modified getGames method to not overwrite game data

--- src/main/java/com/example/demo/service/implementations/GameAdminService.java
+++ src/main/java/com/example/demo/service/implementations/GameAdminService.java
@@ -22,29 +22,35 @@ public class GameAdminService implements IGameAdminService {
     private final IBoardDao boardDao;
     private final IGameDao gameDao;
 
-    public GameAdminService(IBoardDao boardDao, IGameDao gameDao){
+    public GameAdminService(IBoardDao boardDao, IGameDao gameDao) {
 
         this.boardDao = boardDao;
         this.gameDao = gameDao;
     }
 
     @Override
-    public List<Game> getGames() throws ServiceException, DaoException{
+    public List<Game> getGames() throws ServiceException, DaoException {
         // TODO Change (only for demo)... This needs to be ONLY games send back not taking the boards and making them into games
         List<Game> result = new ArrayList<>(gameDao.getGames());
-        for (Board board: boardDao.getBoards()){
-            Game game = new Game();
-            game.name = board.boardName;
-            game.id = board.getGameId();
-            result.add(game);
 
-            game.started = board.getPlayersNumber() > 1;
-            for (int i=0; i < board.getPlayersNumber(); i++){
-                Player player = board.getPlayer(i);
-                User user = new User();
-                user.playerId = player.getPlayerId();
-                user.playerName = player.getName();
-                game.users.add(user);
+        if (result.size() == 0) {
+            for (Board board : boardDao.getBoards()) {
+                Game game = new Game();
+                game.name = board.boardName;
+                game.id = board.getGameId();
+                result.add(game);
+
+                game.started = board.getPlayersNumber() > 1;
+                for (int i = 0; i < board.getPlayersNumber(); i++) {
+                    Player player = board.getPlayer(i);
+                    User user = new User();
+                    user.setPlayerId(player.getPlayerId());
+                    user.setPlayerName(player.getName());
+                    user.setPlayerColor(player.getColor());
+                    user.setGameId(player.board.getGameId());
+                    game.users.add(user);
+                }
+                gameDao.addGame(game);
             }
         }
         return result;
@@ -60,5 +66,7 @@ public class GameAdminService implements IGameAdminService {
         //return "";
     }
 
-
+    public void updateUser(User user) {
+        gameDao.updateUser(user);
+    }
 }
